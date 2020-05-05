4a2804a68c3196cbd7ad4ecf6347941cd4cf1a19
Markus Stenberg <mstenber@aiven.io>
2020-05-05 14:40:19 +0300

    node: Added UT, valid until timestamp

--- tests/node/test_node_lock.py
+++ tests/node/test_node_lock.py
@@ -3,24 +3,6 @@ Copyright (c) 2020 Aiven Ltd
 See LICENSE for details
 """
 
-from astacus.node.api import router as node_router
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-
-import pytest
-
-
-@pytest.fixture(name="app")
-def fixture_app():
-    app = FastAPI()
-    app.include_router(node_router, prefix="/node", tags=["node"])
-    yield app
-
-
-@pytest.fixture(name="client")
-def fixture_client(app):
-    yield TestClient(app)
-
 
 def test_lock_unlock(client):
     # Play with lock
