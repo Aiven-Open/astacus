2895809d74b2472edcfd30d65bfe6cf19fee8780
Markus Stenberg <mstenber@aiven.io>
2020-06-10 09:35:54 +0300

    Get rid of the large-scale locking of NodeState

--- astacus/node/api.py
+++ astacus/node/api.py
@@ -11,8 +11,6 @@ from astacus.common import ipc
 from enum import Enum
 from fastapi import APIRouter, Depends, HTTPException
 
-import time
-
 router = APIRouter()
 
 
@@ -25,31 +23,32 @@ class OpName(str, Enum):
 
 @router.post("/lock")
 def lock(locker: str, ttl: int, state: NodeState = Depends(node_state)):
-    if state.is_locked:
-        raise HTTPException(status_code=409, detail="Already locked")
-    state.locked = True
-    state.locker = locker
-    state.locked_until = time.monotonic() + ttl
+    with state.mutate_lock:
+        if state.is_locked:
+            raise HTTPException(status_code=409, detail="Already locked")
+        state.lock(locker=locker, ttl=ttl)
     return {"locked": True}
 
 
 @router.post("/relock")
 def relock(locker: str, ttl: int, state: NodeState = Depends(node_state)):
-    if not state.is_locked:
-        raise HTTPException(status_code=409, detail="Not locked")
-    if state.locker != locker:
-        raise HTTPException(status_code=403, detail="Locked by someone else")
-    state.locked_until = time.monotonic() + ttl
+    with state.mutate_lock:
+        if not state.is_locked:
+            raise HTTPException(status_code=409, detail="Not locked")
+        if state.is_locked != locker:
+            raise HTTPException(status_code=403, detail="Locked by someone else")
+        state.lock(locker=locker, ttl=ttl)
     return {"locked": True}
 
 
 @router.post("/unlock")
 def unlock(locker: str, state: NodeState = Depends(node_state)):
-    if not state.is_locked:
-        raise HTTPException(status_code=409, detail="Already unlocked")
-    if state.locker != locker:
-        raise HTTPException(status_code=403, detail="Locked by someone else")
-    state.locked = False
+    with state.mutate_lock:
+        if not state.is_locked:
+            raise HTTPException(status_code=409, detail="Already unlocked")
+        if state.is_locked != locker:
+            raise HTTPException(status_code=403, detail="Locked by someone else")
+        state.unlock()
     return {"locked": False}
 
 
--- astacus/node/state.py
+++ astacus/node/state.py
@@ -11,8 +11,9 @@ By design it cannot be persisted to disk, but e.g. op_info can be if necessary.
 from astacus.common import utils
 from astacus.common.op import OpState
 from dataclasses import dataclass
-from fastapi import Depends, Request
+from fastapi import Request
 from threading import Lock
+from typing import Optional
 
 import time
 
@@ -20,29 +21,33 @@ APP_KEY = "node_state"
 APP_LOCK_KEY = "node_lock"
 
 
-@dataclass
-class NodeState(OpState):
-    locked: bool = False
+class LockEntry(utils.AstacusModel):
     locker: str = ""
     locked_until: float = 0
 
+
+@dataclass
+class NodeState(OpState):
+    mutate_lock = Lock()
+    _lock: Optional[LockEntry] = None
+
     @property
     def is_locked(self):
-        if self.locked:
-            if time.monotonic() > self.locked_until:
-                self.locked = False
-        return self.locked
+        lock = self._lock
+        if not lock:
+            return False
+        if time.monotonic() > lock.locked_until:
+            return False
+        return lock.locker
 
     @property
     def still_locked_callback(self):
-        original_locker = self.locker
+        original_lock = self._lock
+        assert original_lock
 
         def _gen():
-            still_locked = self.is_locked
-            while still_locked:
-                if not self.is_locked or self.locker != original_locker:
-                    still_locked = False
-                yield still_locked
+            while self.is_locked == original_lock.locker:
+                yield True
             while True:
                 yield False
 
@@ -53,27 +58,13 @@ class NodeState(OpState):
 
         return _fun
 
+    def lock(self, *, locker, ttl):
+        self._lock = LockEntry(locker=locker, locked_until=time.monotonic() + ttl)
 
-def raw_node_state(request: Request) -> NodeState:
+    def unlock(self):
+        assert self._lock
+        self._lock = None
+
+
+def node_state(request: Request) -> NodeState:
     return utils.get_or_create_state(request=request, key=APP_KEY, factory=NodeState)
-
-
-def node_lock(request: Request) -> Lock:
-    """Acquire Lock to have access to NodeState
-
-    It is intentionally not RLock, as depends are resolved in main
-    asyncio event loop thread, and due to that, every request could
-    acquire same RLock without issues. Plain old Lock works, but care
-    should be taken in not trying to lock multiple times as that will
-    deadlock.
-
-    For longer requests combination of raw_node_state + node_lock
-    + with (only in critical sections) in the different thread should
-    be used.
-    """
-    return utils.get_or_create_state(request=request, key=APP_LOCK_KEY, factory=Lock)
-
-
-def node_state(state: NodeState = Depends(raw_node_state), lock: Lock = Depends(node_lock)):
-    with lock:
-        yield state
