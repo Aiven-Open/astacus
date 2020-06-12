0394c05289a68901e708d81672b8b9cc0a63986b
Markus Stenberg <mstenber@aiven.io>
2020-06-12 14:32:12 +0300

    node: Fixed failure state reporting (and added UT for it)

--- tests/unit/node/test_node_snapshot.py
+++ tests/unit/node/test_node_snapshot.py
@@ -5,6 +5,7 @@ See LICENSE for details
 
 from astacus.common import ipc, utils
 from astacus.common.progress import Progress
+from astacus.node.snapshot import SnapshotOp
 
 import pytest
 
@@ -98,3 +99,28 @@ def test_api_snapshot_and_upload(client, mocker):
     response = m.call_args[1]["data"]
     result = ipc.SnapshotUploadResult.parse_raw(response)
     assert result.progress.finished_successfully
+
+
+def test_api_snapshot_error(client, mocker):
+    req_json = {"root_globs": ["*"]}
+    response = client.post("/node/lock?locker=x&ttl=10")
+    assert response.status_code == 200, response.json()
+    response = client.post("/node/snapshot", json=req_json)
+    assert response.status_code == 200, response.json()
+    status_url = response.json()["status_url"]
+
+    # Now, make sure if it fails it produces sane enough sounding result
+    def _fun(self):
+        raise ValueError("muah")
+
+    mocker.patch.object(SnapshotOp, "snapshot", new=_fun)
+    with pytest.raises(ValueError):
+        # The fact that it propagates here immediately kind of sucks
+        response = client.post("/node/snapshot", json=req_json)
+    assert status_url.endswith('/1')
+    failed_op_status_url = status_url[:-2] + '/2'
+    response = client.get(failed_op_status_url)
+    assert response.status_code == 200, response.json()
+    progress = response.json()["progress"]
+    assert progress["failed"]
+    assert progress["final"]
