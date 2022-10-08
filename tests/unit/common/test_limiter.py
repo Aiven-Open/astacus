e6917c208c4c1eabf527b2abc49bdbc521e99326
Kevin Michel <kevin.michel@aiven.io>
2022-10-08 12:14:00 +0200

    common: add helper for limited gather
    Wraps asyncio.gather and Limiter together
    to directly run a set of tasks concurrently with a maximum number of concurrent tasks.

--- tests/unit/common/test_limiter.py
+++ tests/unit/common/test_limiter.py
@@ -3,7 +3,7 @@ Copyright (c) 2021 Aiven Ltd
 See LICENSE for details
 """
 
-from astacus.common.limiter import Limiter
+from astacus.common.limiter import gather_limited, Limiter
 from typing import Sequence
 
 import asyncio
@@ -32,5 +32,33 @@ async def test_limiter(limit: int, expected_trace: Sequence[str]) -> None:
         limiter.run(add_trace("s1", 0.1, "e1")),
         limiter.run(add_trace("s2", 0.01, "e2")),
         limiter.run(add_trace("s3", 0.03, "e3")),
+    )
+    assert trace == expected_trace
+
+
+@pytest.mark.parametrize(
+    "limit,expected_trace",
+    [
+        (1, ["s1", "e1", "s2", "e2", "s3", "e3"]),
+        (2, ["s1", "s2", "e2", "s3", "e3", "e1"]),
+        (3, ["s1", "s2", "s3", "e2", "e3", "e1"]),
+    ],
+)
+@pytest.mark.asyncio
+async def test_gather_limited(limit: int, expected_trace: Sequence[str]) -> None:
+    trace = []
+
+    async def add_trace(start: str, sleep: float, stop: str):
+        trace.append(start)
+        await asyncio.sleep(sleep)
+        trace.append(stop)
+
+    await gather_limited(
+        limit,
+        [
+            add_trace("s1", 0.1, "e1"),
+            add_trace("s2", 0.01, "e2"),
+            add_trace("s3", 0.03, "e3"),
+        ],
     )
     assert trace == expected_trace
