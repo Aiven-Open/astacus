a2a6519d9bfe7ced1ec924b0cd812cfeef0f4e2b
Kevin Michel <kevin.michel@aiven.io>
2022-11-28 19:07:14 +0100

    Increase EMBEDDED_FILE_SIZE from 100 to 200 bytes
    Many ClickHouse metadata files (one per part) are exactly 185 bytes,
    we can save some object storage requests by making sure we embed
    them in the backup manifest instead of uploading each one of them.
    
    The backup manifest will be larger.

--- astacus/common/magic.py
+++ astacus/common/magic.py
@@ -10,7 +10,7 @@ ASTACUS_DEFAULT_PORT = 5515  # random port not assigned by IANA
 ASTACUS_TMPDIR = ".astacus"
 
 # Hexdigest is 32 bytes, so something orders of magnitude more at least
-EMBEDDED_FILE_SIZE = 100
+EMBEDDED_FILE_SIZE = 200
 
 
 class LockCall(str, Enum):
