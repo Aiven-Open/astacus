a23296de3b79aae9d7672a8539ffca1661468b45
Dmitry Potepalov <dmitry.potepalov@aiven.io>
2023-10-31 15:11:14 +0100

    Implement delta backup retention
    Teach ComputeKeptBackups step to keep delta backup manifests as well.
    Keep only the deltas that can be referenced by a base backup manifest
    (are newer than a base backup manifest).

--- tests/unit/coordinator/plugins/test_base.py
+++ tests/unit/coordinator/plugins/test_base.py
@@ -14,8 +14,11 @@ from astacus.coordinator.config import CoordinatorNode
 from astacus.coordinator.plugins.base import (
     BackupManifestStep,
     ComputeKeptBackupsStep,
+    DeleteBackupAndDeltaManifestsStep,
+    DeleteBackupManifestsStep,
     DeltaManifestsStep,
     ListBackupsStep,
+    ListDeltaBackupsStep,
     ListHexdigestsStep,
     SnapshotReleaseStep,
     SnapshotStep,
@@ -96,7 +99,7 @@ def make_request_check(expected_payload: dict, op_name: str) -> Callable[[httpx.
     return check_request
 
 
-def make_manifest(start: str, end: str) -> ipc.BackupManifest:
+def make_manifest(start: str, end: str, *, filename: str = "") -> ipc.BackupManifest:
     manifest = ipc.BackupManifest(
         start=datetime.datetime.fromisoformat(start),
         end=datetime.datetime.fromisoformat(end),
@@ -104,8 +107,14 @@ def make_manifest(start: str, end: str) -> ipc.BackupManifest:
         snapshot_results=[],
         upload_results=[],
         plugin=Plugin.files,
+        filename=filename,
     )
     return manifest
+
+
+def named_manifest(filename: str) -> ipc.BackupManifest:
+    some_isoformat = "2020-01-05T15:30Z"
+    return make_manifest(some_isoformat, some_isoformat, filename=filename)
 
 
 @pytest.mark.asyncio
@@ -197,6 +206,129 @@ async def test_compute_kept_backups(
     with mock.patch.object(utils, "now", lambda: half_a_day_after_last_backup):
         result = await step.run_step(cluster=single_node_cluster, context=context)
     assert {b.filename for b in result} == kept_backups
+
+
+BACKUPS_FOR_DELTA_RETENTION_TEST = {
+    "delta-0": make_manifest("2020-01-01T03:30Z", "2020-01-01T03:40Z"),
+    "b1": make_manifest("2020-01-01T11:00Z", "2020-01-01T13:00Z"),
+    "b2": make_manifest("2020-01-02T11:00Z", "2020-01-02T13:00Z"),
+    "delta-231": make_manifest("2020-01-02T13:30Z", "2020-01-02T13:40Z"),
+    "delta-232": make_manifest("2020-01-02T15:30Z", "2020-01-02T15:40Z"),
+    "b3": make_manifest("2020-01-03T11:00Z", "2020-01-03T13:00Z"),
+    "b4": make_manifest("2020-01-04T11:00Z", "2020-01-04T13:00Z"),
+    "delta-451": make_manifest("2020-01-04T15:30Z", "2020-01-04T15:40Z"),
+    "b5": make_manifest("2020-01-05T11:00Z", "2020-01-05T13:00Z"),
+    "delta-561": make_manifest("2020-01-05T15:30Z", "2020-01-05T15:40Z"),
+}
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "explicit_delete,expected_kept_backups",
+    [
+        ([], {"b1", "b2", "delta-231", "delta-232", "b3", "b4", "delta-451", "b5", "delta-561"}),
+        (["b1"], {"b2", "delta-231", "delta-232", "b3", "b4", "delta-451", "b5", "delta-561"}),
+        (["b1", "b2"], {"b3", "b4", "delta-451", "b5", "delta-561"}),
+        (["b1", "b2", "b3"], {"b4", "delta-451", "b5", "delta-561"}),
+        (["b1", "b2", "b3", "b4"], {"b5", "delta-561"}),
+        (["b1", "b2", "b3", "b4", "b5"], set()),
+    ],
+)
+async def test_compute_kept_deltas(
+    explicit_delete: Sequence[str],
+    expected_kept_backups: AbstractSet[str],
+    single_node_cluster: Cluster,
+    context: StepsContext,
+):
+    async_json_storage = AsyncJsonStorage(
+        storage=MemoryJsonStorage(items={k: v.json() for k, v in BACKUPS_FOR_DELTA_RETENTION_TEST.items()})
+    )
+    keep_all_retention = ipc.Retention(minimum_backups=len(BACKUPS_FOR_DELTA_RETENTION_TEST))
+    context.set_result(ListBackupsStep, {"b1", "b2", "b3", "b4", "b5"})
+    context.set_result(ListDeltaBackupsStep, {"delta-0", "delta-231", "delta-232", "delta-451", "delta-561"})
+    step = ComputeKeptBackupsStep(
+        json_storage=async_json_storage,
+        retention=keep_all_retention,
+        explicit_delete=explicit_delete,
+        retain_deltas=True,
+    )
+    kept_backups = await step.run_step(single_node_cluster, context)
+    assert {b.filename for b in kept_backups} == expected_kept_backups
+
+
+@pytest.mark.asyncio
+async def test_delete_backup_manifests(single_node_cluster: Cluster, context: StepsContext) -> None:
+    json_items: dict[str, str] = {
+        "b1": "",
+        "d1": "",
+        "b2": "",
+    }
+    async_json_storage = AsyncJsonStorage(storage=MemoryJsonStorage(items=json_items))
+    context.set_result(ListBackupsStep, {"b1", "b2"})
+    context.set_result(ComputeKeptBackupsStep, [named_manifest("b2")])
+    step = DeleteBackupManifestsStep(async_json_storage)
+    deleted_backups = await step.run_step(single_node_cluster, context)
+    assert deleted_backups == {"b1"}
+    assert set(json_items.keys()) == {"d1", "b2"}
+
+
+@dataclasses.dataclass
+class DeleteBackupManifestsParam:
+    all_backups: set[str]
+    all_deltas: set[str]
+    kept_backups: Sequence[ipc.BackupManifest]
+    expected_deleted: set[str]
+    expected_remaining: set[str]
+    test_id: str
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "p",
+    [
+        DeleteBackupManifestsParam(
+            test_id="both base and deltas",
+            all_backups={"b1", "b2"},
+            all_deltas={"d1", "d2"},
+            kept_backups=[named_manifest("b2"), named_manifest("d2")],
+            expected_deleted={"b1", "d1"},
+            expected_remaining={"b2", "d2"},
+        ),
+        DeleteBackupManifestsParam(
+            test_id="no deltas",
+            all_backups={"b1", "b2"},
+            all_deltas=set(),
+            kept_backups=[named_manifest("b2")],
+            expected_deleted={"b1"},
+            expected_remaining={"b2"},
+        ),
+    ],
+    ids=lambda p: p.test_id,
+)
+async def test_delete_backup_and_delta_manifests(
+    single_node_cluster: Cluster, context: StepsContext, p: DeleteBackupManifestsParam
+) -> None:
+    json_items = {b: named_manifest(b).json() for b in (p.all_backups | p.all_deltas)}
+    async_json_storage = AsyncJsonStorage(storage=MemoryJsonStorage(items=json_items))
+    context.set_result(ListBackupsStep, p.all_backups)
+    context.set_result(ListDeltaBackupsStep, p.all_deltas)
+    context.set_result(ComputeKeptBackupsStep, p.kept_backups)
+    step = DeleteBackupAndDeltaManifestsStep(async_json_storage)
+    deleted_backups = await step.run_step(single_node_cluster, context)
+    assert deleted_backups == p.expected_deleted
+    assert set(json_items.keys()) == p.expected_remaining
+
+
+@pytest.mark.asyncio
+async def test_delete_backup_and_delta_manifests_raises_when_delta_steps_are_missing(
+    single_node_cluster: Cluster, context: StepsContext
+) -> None:
+    async_json_storage = AsyncJsonStorage(storage=MemoryJsonStorage(items={}))
+    context.set_result(ListBackupsStep, set())
+    context.set_result(ComputeKeptBackupsStep, set())
+    step = DeleteBackupAndDeltaManifestsStep(async_json_storage)
+    with pytest.raises(Exception):
+        await step.run_step(single_node_cluster, context)
 
 
 @pytest.mark.asyncio
