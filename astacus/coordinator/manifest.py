8fd1a212463b630877b0a037ce6549e7f88096c9
Kevin Michel <kevin.michel@aiven.io>
2024-04-21 14:08:57 +0200

    extract sync code from download_backup_manifest
    The sync variant will be directly used in some steps.
    [DDB-951]

--- astacus/coordinator/manifest.py
+++ astacus/coordinator/manifest.py
@@ -3,17 +3,19 @@ Copyright (c) 2021 Aiven Ltd
 See LICENSE for details
 """
 from astacus.common import asyncstorage, ipc
+from astacus.common.storage import JsonStorage
 from starlette.concurrency import run_in_threadpool
 
 
-async def download_backup_manifest(json_storage: asyncstorage.AsyncJsonStorage, backup_name: str) -> ipc.BackupManifest:
-    def download_manifest() -> ipc.BackupManifest:
-        return json_storage.storage.download_json(backup_name, ipc.BackupManifest)
-
-    manifest = await run_in_threadpool(download_manifest)
+def download_backup_manifest_sync(json_storage: JsonStorage, backup_name: str) -> ipc.BackupManifest:
+    manifest = json_storage.download_json(backup_name, ipc.BackupManifest)
     assert not manifest.filename or manifest.filename == backup_name
     manifest.filename = backup_name
     return manifest
+
+
+async def download_backup_manifest(json_storage: asyncstorage.AsyncJsonStorage, backup_name: str) -> ipc.BackupManifest:
+    return await run_in_threadpool(download_backup_manifest_sync, json_storage.storage, backup_name)
 
 
 async def download_backup_min_manifest(json_storage: asyncstorage.AsyncJsonStorage, backup_name: str) -> ipc.ManifestMin:
