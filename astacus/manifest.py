8d5790e7c7d0db965c52018c8e22bc6084f6064a
Dmitry Potepalov <dmitry.potepalov@aiven.io>
2024-10-16 17:03:24 +0200

    Add a manifest describe command
    Add a command that prints some high-level info about the manifest
    without recursing into snapshot contents.

--- astacus/manifest.py
+++ astacus/manifest.py
@@ -7,6 +7,7 @@ without a running Astacus process.
 
 """
 
+from astacus.common import ipc
 from astacus.common.rohmustorage import RohmuConfig, RohmuStorage
 
 import json
@@ -18,6 +19,7 @@ def create_manifest_parsers(parser, subparsers):
     p_manifest.add_argument("-s", "--storage", type=str, help="Storage to use")
     manifest_subparsers = p_manifest.add_subparsers(title="Manifest commands")
     create_list_parser(manifest_subparsers)
+    create_describe_parser(manifest_subparsers)
 
 
 def create_list_parser(subparsers):
@@ -25,11 +27,39 @@ def create_list_parser(subparsers):
     p_list.set_defaults(func=_run_list)
 
 
+def create_describe_parser(subparsers):
+    p_describe = subparsers.add_parser("describe", help="Print info/statistics from a backup manifest")
+    p_describe.add_argument("manifest", type=str, help="Manifest object name (can be obtained by running manifest list)")
+    p_describe.set_defaults(func=_run_describe)
+
+
 def _run_list(args):
     rohmu_storage = _create_rohmu_storage(args.config, args.storage)
     json_names = rohmu_storage.list_jsons()
     for _json in json_names:
         print(_json)
+
+
+def _run_describe(args):
+    rohmu_storage = _create_rohmu_storage(args.config, args.storage)
+    manifest = rohmu_storage.download_json(args.manifest, ipc.BackupManifest)
+    print(manifest.plugin, "manifest", args.manifest)
+    print("===============================")
+    print("Started at:", manifest.start.isoformat())
+    print("Finished at:", manifest.end.isoformat())
+    print("Attempts:", manifest.attempt)
+    for i, snapshot_result in enumerate(manifest.snapshot_results):
+        print(
+            f"Snapshot #{i} created at {snapshot_result.start.isoformat()}",
+            f"contains {snapshot_result.files} files ({snapshot_result.total_size} bytes)",
+        )
+    for i, upload_result in enumerate(manifest.upload_results):
+        print(f"Node #{i} uploaded {upload_result.total_stored_size} bytes out of {upload_result.total_size}")
+    if manifest.tiered_storage_results:
+        print(
+            f"References {manifest.tiered_storage_results.n_objects} objects in tiered storage",
+            f"totalling {manifest.tiered_storage_results.total_size_bytes} bytes",
+        )
 
 
 def _create_rohmu_storage(config_path: str, storage: str) -> RohmuStorage:
